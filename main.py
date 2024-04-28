1095bd3b9e7f8045ec7984c615735d418c9946cf
Werikly <mi20090803@gmail.com>
2024-04-28 02:39:47 -0700

    added new logic

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ turtle.screensize(480, 480, bg="#ccc")
 turtle.tracer(0)
 turtle.hideturtle()
 
-turtle.done()
+def draw_square(x, y, lenght):
+    uzbek.penup()
+    uzbek.home()
+    uzbek.goto(x,y)
+    uzbek.color("red")
+    uzbek.pendown()
+
+    for storona in range(4):
+      uzbek.forward(lenght)
+      uzbek.right(90)
+
+
+turtle.done
