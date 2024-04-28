e774341d2e32cbde01cd42644ace744fd27b3afc
Werikly <mi20090803@gmail.com>
2024-04-28 03:05:35 -0700

    abubaads

--- main.py
+++ main.py
@@ -19,5 +19,21 @@ def draw_square(x, y, lenght):
       uzbek.forward(lenght)
       uzbek.right(90)
 
+def drav_watchface(length, rotation):
+   uzbek.penup()
+   uzbek.home()
+   uzbek.color("black")
+   uzbek.forward(150)
+   uzbek.pendown()
+   uzbek.forward(length)
+
+for i in range(0, 360, 30):
+   uzbek.right(i)
+   drav_watchface(20)
+
+for i in range(0, 360, 6):
+   uzbek.right(i)
+   drav_watchface(10)
+
 
 turtle.done
